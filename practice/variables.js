c59fce26c46eeec9f3c7f5773cca50947abb9769
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-10-31 07:02:42 +0000

    updated

---
+++ practice/variables.js
@@ -0,0 +1,12 @@
+const name="kiran gajjana";
+let email="kiran.gajjana@gmail.com";
+email="kirangajjana@gmail.com";
+console.log(email);
+email="m210008@nitsikkim.ac.in";
+console.log(name);
+console.log(email);
+var names="hurake";
+console.log(names);
+var names="varun";
+console.log(names)
+console.log(email)
