794ea8c3bbdd18512e707ec073e282438f804bb2
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-10-31 11:04:20 +0000

    premitive datatypes stored in stack

---
+++ practice/stack.js
@@ -0,0 +1,10 @@
+//stack (primitive data type)
+//when we copy or pass any variable as a argument we will pass a copy of the variable which will
+// not effect the actual variable as if we do any operation on int
+let a=5;
+let b=a;
+console.log(b);
+console.log(a);
+a=25;
+console.log(a);
+console.log(b);
