552b38b1ecc7e3b6c450f1fdaa8db8c87619b34b
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-10-31 07:41:17 +0000

    Primitive DataTypes

---
+++ practice/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"//treats all the code as a newer version of the code
+//if we use primitive datatypes and if we want to assign or pass that to function then 
+//we will give the copy of the varaible we the change in the main variable will not effect the change in the copy variable 
+let a=5;
+let b=a;
+console.log(b);
+console.log(a);
+console.log("now we will se the power of primitive data types");
+a=10;
+console.log(a);
+console.log(b);
