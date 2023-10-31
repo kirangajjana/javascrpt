7cf0d21ef6abfdfb9703be494ea867b2fe760038
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-10-31 08:12:28 +0000

    conversion of datatypes

---
+++ practice/conversionoperations.js
@@ -0,0 +1,18 @@
+let a="5abc";
+let c="kiran";
+console.log(c);
+console.log(typeof c);
+let bola=Boolean(c);
+console.log(bola);
+console.log(typeof bola);
+let b=1;
+console.log(a);
+console.log(typeof a);
+let number=Number(a);
+console.log(number);
+console.log(typeof number);
+console.log(b)
+console.log(typeof b);
+let bol=Boolean(b);
+console.log(bol);
+console.log(typeof bol);
