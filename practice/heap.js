860ea3dc4c9ffb91709dcf51029ce68de0bea3c8
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-10-31 11:11:32 +0000

    non primitive datatypes

---
+++ practice/heap.js
@@ -0,0 +1,10 @@
+let kiran={
+    email:"m210008@nitsikkim.ac.in",
+    age:18,
+    city:"hyderabad"
+}
+let teja=kiran;
+console.log(teja);
+teja.email="teja.gajjana@gmail.com";
+console.log(teja);
+console.log(kiran);
