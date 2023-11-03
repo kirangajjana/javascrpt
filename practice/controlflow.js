305772a74ac93729988667adcfaad505afb196f0
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-03 18:40:25 +0000

    updated

---
+++ practice/controlflow.js
@@ -0,0 +1,11 @@
+const loggedin=true;
+if(loggedin)
+{
+    var name="kiran gajjana";
+    console.log("you are welcome");
+    console.log(`hello mr.${name}`);
+}
+else{
+    console.log("sorry please try again");
+}
+console.log(`hello mr.${name}`);
