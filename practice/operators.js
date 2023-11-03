bb5e7d102bb72144f430b89ced2f2990d0bfc68b
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-03 17:32:11 +0000

    update

---
+++ practice/operators.js
@@ -0,0 +1,35 @@
+// Arithematic operators
+let a=5;
+let b=10;
+console.log("a+b= ",a+b);
+console.log("a-b= ",a-b);
+console.log("a*b= ",a*b);
+console.log("a,b= ",a/b);
+console.log("a%b= ",a%b);
+console.log("a**b= ",a**b);
+console.log("pre increpemet operator",++a);
+console.log("pre decrement operator",--a);
+console.log("post increment operator",a++);
+console.log("post decrement operators",a--);
+console.log(a);
+// assignment operators
+console.log(a==b);
+console.log(a+=a);
+console.log(a-=a);
+console.log(a);
+console.log(a*=a);
+console.log(a/=a);
+console.log(a**=a);
+console.log(a%=a);
+//comparision operators
+let comp1=10;
+let comp2=11;
+console.log("comp1==comp2",comp1==comp2);
+console.log("comp1===comp2",comp1===comp2);
+console.log("comp1!=comp2",comp1!=comp2);
+
+console.log("comp1!=comp2",comp1!==comp2);
+//logical operators
+console.log("logical operator",comp1>20 && comp2>3);
+console.log("logical operator",comp1>20 || comp2>3);
+console.log(!1);
