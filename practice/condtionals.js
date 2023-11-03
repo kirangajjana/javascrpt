11861c593e654c79f950141a8ba286363e31cfaa
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-03 18:11:08 +0000

    conditionals

---
+++ practice/condtionals.js
@@ -0,0 +1,12 @@
+const name="kiran gajjana";
+if(name=="kiran gajjana")
+{
+    console.log("hello welcome to the new application mr.",name);
+}
+else{
+    console.log("sorry you weere not eligible to use this application");
+}
+//ternary operators
+let comp1=10;
+let comp2=20;
+console.log(comp1>comp2?"hello world":"sorry"); 
