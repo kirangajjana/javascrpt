d6231a0c7536913bf15d0cd1552cf0f6d0c83ff1
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-03 17:01:15 +0000

    updated

---
+++ practice/practice.js
@@ -0,0 +1,17 @@
+let a="kiran gajjana";
+a=a+5;
+console.log(a);
+console.log(typeof a);
+const a1={
+    name:"kiran gajjana",
+    sec:1
+}
+a1['new']="harhsita";
+a1['name']="harshita patnaik";
+console.log(a1);
+a1={
+    name:"abhishek",
+    sec:1,
+    college:"national institute of technology sikkim"
+}
+console.log(a1);
