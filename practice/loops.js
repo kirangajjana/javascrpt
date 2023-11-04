b2b5fc8ef6093c6c00a5cbd5136d79f5ac52296e
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 06:18:07 +0000

    loops

---
+++ practice/loops.js
@@ -0,0 +1,8 @@
+let sum=0;
+for(let a=0;a<=30;a++)
+{
+    console.log(`hello world ${a}`);
+    sum+=a;
+}
+console.log(`hello kiran gajjana`);
+console.log(`the total sum of the months are ${sum}`);
