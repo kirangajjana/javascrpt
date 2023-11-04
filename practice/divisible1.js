d28af0e0817ddc5338a47ea017386fc7c2a28de6
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 05:48:41 +0000

    update

---
+++ practice/divisible1.js
@@ -0,0 +1,8 @@
+let a=7;
+if(a%2==0 && a%3==0)
+{
+    console.log(`the given number ${a} is divisible by 2 and 3`);
+}
+else{
+    console.log(`the givven number${a} is not divisibe by both 2 and 3`);
+}
