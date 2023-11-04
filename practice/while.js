3544b73150dddc355b3f98841253fede6d44aea3
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 07:28:19 +0000

    update

---
+++ practice/while.js
@@ -0,0 +1,13 @@
+function sum(x,y)
+{
+    return a+b;
+}
+const hello=()=>
+{
+    console.log("hello kiran gajjana");
+    console.log("hello ella unnaru andharu bagunnara");
+}
+let a=5;
+let b=10;
+console.log(`the addition of two numbers ${sum(a,b)}`);
+hello();
