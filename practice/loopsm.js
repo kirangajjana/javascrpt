b6d419f1c97ba3639707aebd640e56fbcf4bb3dc
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 06:30:48 +0000

    update

---
+++ practice/loopsm.js
@@ -0,0 +1,18 @@
+let mtech={
+    abhishek:100,
+    deepak:99,
+    saket:98,
+    yetendra:97,
+    sumansom:96,
+    shubhanisingh:95,
+    sohambiswas:94,
+    kirangajjana:93,
+    dorothybarman:92,
+    sreyeshikarmakar:91,
+    anuragmaurya:90,
+    bipalkhanal:89
+}
+for(a in mtech)
+{
+    console.log(`hello mr.${a} and marks = ${mtech[a]}`);
+}
