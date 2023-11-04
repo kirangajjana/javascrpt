896c808fd5603f0607fbe5f88c1c3ae0514c4e6b
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 05:45:03 +0000

    divisible

---
+++ practice/divisible.js
@@ -0,0 +1,12 @@
+let a=10;
+if(a%2==0)
+{
+    console.log("the given number is an even number");
+}
+else if(a%3==0)
+{
+    console.log("the given number is an odd number");
+}
+else{
+    console.log("please enter correct key for further references");
+}
