618bac4c53b86038ae2535ff7dcdf39637b1abf3
kirangajjana <44581291+kirangajjana@users.noreply.github.com>
2023-11-04 05:41:21 +0000

    update

---
+++ practice/conditionalsp.js
@@ -0,0 +1,8 @@
+let age=21;
+if(age>=10 && age<=20)
+{
+    console.log("you can access the application now");
+}
+else{
+    console.log("sorry you cant access this application now");
+}
---
+++ practice/switchprac.js
@@ -0,0 +1,17 @@
+let name="Abhishek";
+switch(name)
+{
+    case "Abhishek":
+        {
+            console.log(`hello abhishek`);
+            break;
+        }
+    case "deepak":
+        {
+            console.log("hello deepak");
+        }
+    default:
+        {
+            console.log("please enter correct key for further reference");
+        }        
+}
